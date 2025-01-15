7dfd45ec74d6a4d3feb42c2d2cdf021ecb9df195
Matthew Grogan <mvgnyc@gmail.com>
2025-01-15 14:15:15 -0500

    3pm

--- src/scenes/three_point_molly/three_point_molly.ts
+++ src/scenes/three_point_molly/three_point_molly.ts
@@ -1,6 +1,8 @@
 import { makeProject } from "@motion-canvas/core";
 import "../../global.css";
 
+import audio from "../../../audio/DD-THREE_PT_MOLLY-MAXODDS-15_00108000.flac"
+
 import DD_TOC_01_Strategy from "../titles/DD_TOC_01_Strategy?scene";
 import DD_TOC_02_Simulation from "../titles/DD_TOC_02_Simulation?scene";
 import DD_TOC_03_Dice_And_Gameflow from "../titles/DD_TOC_03_Dice_And_Gameflow?scene";
@@ -60,4 +62,5 @@ export default makeProject({
 
     // DD_TOC_08_Strategy_Score,
   ],
+  audio: audio
 });
