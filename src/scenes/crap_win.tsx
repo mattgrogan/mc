e1b7a719cee4aad1a96ecce1fb4759c6208b56f2
Matthew Grogan <matt.grogan@gia.edu>
2025-03-14 12:27:13 -0400

    added example win conditions

--- src/scenes/crap_win.tsx
+++ src/scenes/crap_win.tsx
@@ -12,14 +12,27 @@ export default makeScene2D(function* (view) {
       tableProps={{ height: 400, width: 3300 }}
       valueRowHeight={200}
       labelProps={{ fontSize: 100, fill: "white" }}
+      extensionLength={100}
       // TODO: how can I style the value props?
     />
   );
 
   yield* winconditions().update([
-    { throw: "8H", winloss: -60 },
-    { throw: "5", winloss: 50 },
-    { throw: "6E", winloss: 90 },
+    { throw: "2", winloss: 10 },
+    { throw: "3", winloss: 10 },
+    { throw: "4E", winloss: -2010 },
+    { throw: "4H", winloss: -1010 },
+    { throw: "5", winloss: 0 },
+    { throw: "6E", winloss: 0 },
+    { throw: "6H", winloss: 0 },
+    { throw: "7", winloss: 2010 },
+    { throw: "8E", winloss: 0 },
+    { throw: "8H", winloss: 0 },
+    { throw: "9", winloss: -1510 },
+    { throw: "10E", winloss: 0 },
+    { throw: "10H", winloss: 0 },
+    { throw: "11", winloss: -10 },
+    { throw: "12", winloss: 0 },
   ]);
 
   // yield* crap().valueColumnWidth(120, 1);
@@ -70,11 +83,21 @@ export default makeScene2D(function* (view) {
   yield* winconditions().highlight(2, 4, { fill: "yellow", opacity: 1 });
 
   yield* winconditions().update([
-    { throw: "2H", winloss: -50 },
-    { throw: "2E", winloss: -60 },
-    { throw: "5", winloss: 40 },
-    { throw: "6E", winloss: -90 },
-    { throw: "6H", winloss: -90 },
+    { throw: "2", winloss: 10 },
+    { throw: "3", winloss: 10 },
+    { throw: "4E", winloss: 0 },
+    { throw: "4H", winloss: 0 },
+    { throw: "5", winloss: 0 },
+    { throw: "6E", winloss: -49 },
+    { throw: "6H", winloss: 149 },
+    { throw: "7", winloss: -10 },
+    { throw: "8E", winloss: 0 },
+    { throw: "8H", winloss: 0 },
+    { throw: "9", winloss: 0 },
+    { throw: "10E", winloss: 0 },
+    { throw: "10H", winloss: 0 },
+    { throw: "11", winloss: -10 },
+    { throw: "12", winloss: 0 },
   ]);
 
   yield* waitFor(1);
