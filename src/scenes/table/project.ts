000e3f3550f4bb6ae5cc620e3334b20f1d1d0e05
Matthew Grogan <matt.grogan@gia.edu>
2025-05-14 11:48:23 -0400

    Update throws to profit

--- src/scenes/table/project.ts
+++ src/scenes/table/project.ts
@@ -2,9 +2,8 @@ import { makeProject } from "@motion-canvas/core";
 import "../../global.css";
 
 import scene from "./scene?scene";
+import sonic_scene from "./sonic_scene?scene";
 
 export default makeProject({
-  scenes: [
-    scene
-  ],
+  scenes: [scene, sonic_scene],
 });
